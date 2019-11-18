6f0d63a96018149316f23cc2c2599cd0fd58f867
Vadym Lazariev <vadymlazariev@gmail.com>
2019-11-18 12:27:59 +0200

    added shop page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+
 import HomePage from "./pages/homepage/hompage.component";
+import ShopPage from "./pages/shop/shop.page.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
